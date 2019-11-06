debab6eca066133b31df4baba46559e2836c94f7
abhidp <abhi358@gmail.com>
2019-11-06 12:37:51 +1100

    create

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,18 @@
 /// <reference types="Cypress" />
 
+const loggedIn = () => {
+	cy.location('pathname').should('eq', '/employees')
+			.get('#greetings')
+			.should('be.visible')
+			.should('have.text', `Hello ${Cypress.env('username')}`)
+}
+
 Cypress.Commands.add('login', () => {
   cy.visit(`${Cypress.env('baseUrl')}/login`)
 			.get('[ng-model="user.name"]').type(Cypress.env('username'))
 			.get('[ng-model="user.password"]').type(Cypress.env('password'))
-      .get('#login-form').submit()
+		.get('#login-form').submit()
+	loggedIn()
 })
 
 
